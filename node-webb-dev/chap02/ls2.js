9a57185eb4113dd86dcbc971617d760b1986506e
harjar <44702232+harjar@users.noreply.github.com>
2026-04-19 21:40:52 +0200

    Mer node från bok på git

---
+++ node-webb-dev/chap02/ls2.js
@@ -0,0 +1,23 @@
+const fs = require('fs').promises;
+async function listFiles() {
+    try {
+        var dir = '.';
+        if (process.argv[2]) dir = process.argv[2];
+        const files = await fs.readdir(dir);
+        for (let fn of files) {
+            console.log(fn);
+        }
+    } catch (err) {
+        console.error(err);
+    }
+}
+listFiles();
+
+(async () => {
+    var dir = '.';
+    if (process.argv[2]) dir = process.argv[2];
+    const files = await fs.readdir(dir);
+    for (let fn of files) {
+        console.log(fn);
+    }
+})().catch(err => { console.error(err); });
