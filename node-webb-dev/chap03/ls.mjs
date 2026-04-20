a5618a15e84b4ecb01808df273bb8a518e1b513d
harjar <44702232+harjar@users.noreply.github.com>
2026-04-20 22:24:15 +0200

    kapite 3 påbörjad

---
+++ node-webb-dev/chap03/ls.mjs
@@ -0,0 +1,8 @@
+import { promises as fs } from 'fs';
+async function listFiles() {
+    const files = await fs.readdir('.');
+    for (const file of files) {
+        console.log(file);
+    }
+}
+listFiles().catch(err => { console.error(err); });
