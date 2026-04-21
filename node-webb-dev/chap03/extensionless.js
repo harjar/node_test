1ae5928a64571d39c3bb76af936a003bc4f2f068
harjar <44702232+harjar@users.noreply.github.com>
2026-04-21 21:56:12 +0200

    chap04, påbörjades, sid 136

---
+++ node-webb-dev/chap03/extensionless.js
@@ -0,0 +1,4 @@
+const simple = require('./simple');
+console.log(simple.hello());
+console.log(`${simple.next()}`);
+console.log(`${simple.next()}`);
---
+++ node-webb-dev/chap03/showdata-es6.mjs
@@ -0,0 +1,5 @@
+import * as data from './data.json';
+console.log(data);
+
+// test node --experimental-modules --experimental-json-modules showdataes6.mjs fungerar ej pga att det inte finns i 
+// node 18.3.0, så istället kör node showdata-es6.mjs, fungerar inte
